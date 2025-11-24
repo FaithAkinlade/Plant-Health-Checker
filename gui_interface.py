d5faa36a2598a970aa80c540d0011c12c88eedfb
prwaghela1 <prwaghela1@gmail.com>
2025-11-24 18:07:04 -0500

    Backend Integration
    Now that the window is working, I connected our backend to the frontend. I imported the PlantDataManager class we made earlier and set it up so that it automatically loads our plants_dataset.csv (this file is yet to be created) file right when the app starts. I did this inside the __init__ method so the data is ready to go immediately. This is better than making the user click a 'Load Data' button every time they open the app.

--- gui_interface.py
+++ gui_interface.py
@@ -1,18 +1,22 @@
-# gui_interface.py (Version 1)
+# gui_interface.py (Update imports and __init__)
 import tkinter as tk
+from tkinter import filedialog, messagebox
+from data_manager import PlantDataManager # <--- Added this
 
 class PlantHealthApp:
-    """
-    The main GUI class for the Plant health checker app.
-    """
     def __init__(self, root):
         self.root = root
         self.root.title("Group 7: Plant Health Checker")
         self.root.geometry("500x400")
 
+        # We load data immediately so the user doesn't have to click a button
+        self.data_manager = PlantDataManager('data/plants_dataset.csv') # <--- Added
+        self.data_manager.load_dataset() # <--- Added
+        self.selected_image_path = None # <--- Added
+
         # Title
         self.label_title = tk.Label(root, text="Plant Health Checker", font=("Arial", 18, "bold"))
         self.label_title.pack(pady=20)
-        
+
         # Instructions
         tk.Label(root, text="Step 1: Upload Image", font=("Arial", 12)).pack(pady=(10, 5))
